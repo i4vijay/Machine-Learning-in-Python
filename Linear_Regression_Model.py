b6e9faaaea050b33edd632bbde43ac5fcc9f4aab
S.VIJAY <36433544+i4vijay@users.noreply.github.com>
2019-04-01 20:08:22 +0530

    Update Linear_Regression_Model.py

--- Linear_Regression_Model.py
+++ Linear_Regression_Model.py
@@ -11,9 +11,10 @@ Y = data.iloc[:,1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state = 0)
 
-
+# Model Building
 from sklearn.linear_model import LinearRegression
 lm = LinearRegression()
 model = lm.fit(X_train, Y_train)
 
+# Predicting the Salary
 prediction = lm.predict(X_test)
