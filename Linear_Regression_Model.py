616b3aeb66d9e002ddc03b562fd1f0c91275a307
S.VIJAY <36433544+i4vijay@users.noreply.github.com>
2019-04-01 20:06:53 +0530

    Add files via upload

---
+++ Linear_Regression_Model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+data = pd.read_csv("Salary_Data.csv")
+X = data.iloc[:,:-1].values
+Y = data.iloc[:,1].values
+
+# Splitting dataset to train and test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state = 0)
+
+
+from sklearn.linear_model import LinearRegression
+lm = LinearRegression()
+model = lm.fit(X_train, Y_train)
+
+prediction = lm.predict(X_test)
